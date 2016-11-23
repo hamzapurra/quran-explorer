5cde2b1f8eb11583d73faf9a970bd636cf2f8e87
Hamza M Purra <gm00cell@gmail.com>
2016-11-23 16:24:49 +0400

    feat: Add ability to get the verses of a surah

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai')
 
 const loadQuran = require('../loadQuran')
-const { getVerse } = require('../index')
+const { getVerse, getSurah } = require('../index')
+const { verseCountBySurah, verseIndexBySurah } = require('../versesBySurah')
 
 describe('getVerse', function() {
   it('should return the first verse', function(done) {
@@ -19,6 +20,24 @@ describe('getVerse', function() {
         expect(verse).to.equal(quran.slice(-1)[0])
         done()
       })
+    })
+  })
+})
+
+describe('getSurah', function() {
+  it('should return the correct verses', function(done) {
+    loadQuran().then(quran => {
+      const promises = [];
+
+      [...Array(114).keys()].forEach(surahIndex => {
+        promises.push(getSurah(surahIndex + 1).then(verses => {
+          const surahStartIndex = verseIndexBySurah[surahIndex]
+          const surahLength = verseCountBySurah[surahIndex]
+          expect(verses).to.deep.equal(quran.slice(surahStartIndex, surahStartIndex + surahLength))
+        }))
+      })
+
+      return Promise.all(promises).then(() => done())
     })
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const loadQuran = require('./loadQuran')
+const { verseIndexBySurah } = require('./versesBySurah')
 
 const getVerse = num => {
   if (num < 1 || num > 6236) {
     return Promise.reject('Verse number needs to be between 1 and 6236.')
   }
+
   return loadQuran().then(quran => quran[num - 1])
+}
+
+const getSurah = num => {
+  if (num < 1 || num > 114) {
+    return Promise.reject('Surah number needs to be between 1 and 114.')
+  }
+
+  const startingIndexOfSurah = verseIndexBySurah[num - 1]
+  const startingIndexOfNextSurah = verseIndexBySurah[num]
+  return loadQuran().then(quran => quran.slice(startingIndexOfSurah, startingIndexOfNextSurah))
 }
 
 module.exports = {
   getVerse,
+  getSurah,
 }
